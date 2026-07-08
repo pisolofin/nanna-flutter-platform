23b4b85a6be7cd83cf5bba93d3b651d35218f495
pisolofin <pisolofin@outlook.com>
2026-07-08 16:27:29 +0200

    Import

--- example/lib/widgets/example-card-list.widget.dart
+++ example/lib/widgets/example-card-list.widget.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:nanna_platform/nanna_platform.dart';
 
 class ExampleCardListWidget extends StatelessWidget {
