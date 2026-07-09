e0ccfc3745d6deaa580f4e510035ecd68b104627
pisolofin <pisolofin@outlook.com>
2026-07-09 15:30:38 +0200

    Code style

--- test/helpers/test-helpers.dart
+++ test/helpers/test-helpers.dart
@@ -4,7 +4,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nanna_platform/nanna_platform.dart';
 
 /// Wraps a widget in a [MaterialApp] and injects a [NaUiTypeScope] with [NaUiType.material].
-Future<void> pumpMaterialNaWidget(WidgetTester tester, Widget child) async {
+Future pumpMaterialNaWidget(WidgetTester tester, Widget child) async {
   await tester.pumpWidget(
     NaUiTypeScope(
       uiTypes: const [NaUiType.material],
@@ -18,7 +18,7 @@ Future<void> pumpMaterialNaWidget(WidgetTester tester, Widget child) async {
 }
 
 /// Wraps a widget in a [CupertinoApp] and injects a [NaUiTypeScope] with [NaUiType.cupertino].
-Future<void> pumpCupertinoNaWidget(WidgetTester tester, Widget child) async {
+Future pumpCupertinoNaWidget(WidgetTester tester, Widget child) async {
   await tester.pumpWidget(
     NaUiTypeScope(
       uiTypes: const [NaUiType.cupertino],
@@ -30,3 +30,4 @@ Future<void> pumpCupertinoNaWidget(WidgetTester tester, Widget child) async {
     ),
   );
 }
+
